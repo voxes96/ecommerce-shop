1b49d6ec4788511ca59325bf151d5c52b53b5106
Szymon Liszka <liszkaszymon@gmail.com>
2020-06-18 23:56:01 +0200

    Add detail view of product

--- shop/urls.py
+++ shop/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:product_id>', views.detail, name='detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def index(request):
     products_list = Product.objects.filter(active=True)
     return render(request, 'shop/index.html', {'products_list': products_list})
+
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'shop/detail.html', {'product': product})
